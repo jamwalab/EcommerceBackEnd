1feffe42ce435a80175394a24450494a22b59002
Abhishek Jamwal <jamwal.ab@gmail.com>
2021-07-16 11:49:14 -0400

    Model associations formed in index

--- models/index.js
+++ models/index.js
@@ -4,13 +4,45 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Products and Category association
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 
-// Categories have many Products
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
-// Products belongToMany Tags (through ProductTag)
+// Products and Tags associations (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: product_id
+});
 
-// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: tag_id
+});
+
+// ProductTag and Product associations 
+
+ProductTag.belongsTo(Product, {
+  foreignKey: product_id
+});
+
+Product.hasMany(ProductTag, {
+  foreignKey: product_id
+})
+
+// ProductTag and Tags associations
+
+ProductTag.belongsTo(Tag, {
+  foreignKey: tag_id
+});
+
+Tag.hasMany(ProductTag, {
+  foreignKey: tag_id
+})
 
 module.exports = {
   Product,
